f158de97a928f7f73ef84460cd892481461e7beb
zylstraj <colbertlove12@gmail.com>
2017-04-04 17:37:27 -0700

    Problems.
    I can see the payload in the action.payload.data.results - but for some
    reason, I'm unable to connect it to state right now.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-const url = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=8c14264ac98b4b1bba489789249fbb81";
-
 export const FETCH_DATA = 'FETCH_DATA';
 
-export function fetchNews(news) {
+export function fetchNews() {
+  const url = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=8c14264ac98b4b1bba489789249fbb81";
   const request = axios.get(url);
   console.log('Request:', request);
   return {
