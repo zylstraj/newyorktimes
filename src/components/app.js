6bc179030d432017d111eb5c53b1d3fb2e15e3c5
zylstraj <colbertlove12@gmail.com>
2017-04-06 11:43:11 -0700

    Reconfiguing Structure
    Created app component - using as a base. I think this will be helpful
    when moving over to ReactRouter

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NewsStories from './news_stories';
+
+const App = () => {
+    return (
+      <div>
+        <NewsStories />
+      </div>
+  )
+}
+export default App;
