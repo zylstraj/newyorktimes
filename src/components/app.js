3f4488a480577a6094163fcb10e1144f855e79d4
zylstraj <colbertlove12@gmail.com>
2017-04-17 15:21:22 -0700

    Rearranging. Settling on some very basic starter colors
    Functionality is pretty much done. I could always add a YouTube or
    Reddit, but just trying to ge ta bunch of little projects done.

--- src/components/app.js
+++ src/components/app.js
@@ -2,15 +2,16 @@ import React from 'react';
 import NewsStories from './news_stories';
 import SearchBar from './search-bar';
 import Header from './Header';
+import styles from './app.css';
 
 const App = () => {
     return (
       <div>
         <Header />
-        <NewsStories />
         <div>
         <SearchBar />
         </div>
+        <NewsStories />
       </div>
   )
 }
--- src/components/weather_list.js
+++ src/components/weather_list.js
@@ -7,10 +7,17 @@ class WeatherList extends React.Component {
     console.log(cityData.list[0].main.temperature);
     return (
       <div key={cityData.city.name} className={styles.cityList}>
-      <h2>Today</h2>
-      <p>{cityData.city.name}</p>
-      <p>Temperature: {cityData.list[0].main.temp}</p>
-      <p>Description: {cityData.list[0].weather[0].description}</p>
+        <p className={styles.cityName}>{cityData.city.name}</p>
+        <div className={styles.cityToday}>
+          <h3>Today</h3>
+          <p className={styles.cityInfo}>Temperature: {cityData.list[0].main.temp}</p>
+          <p className={styles.cityInfo}>Description: {cityData.list[0].weather[0].description}</p>
+          </div>
+          <div className={styles.cityTomorrow}>
+          <h3>Tomorrow</h3>
+          <p className={styles.cityInfo}>Temperature: {cityData.list[5].main.temp}</p>
+          <p className={styles.cityInfo}>Description: {cityData.list[5].weather[0].description}</p>
+        </div>
       </div>
     )
   }
