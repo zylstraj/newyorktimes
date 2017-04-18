f910199eb5c44921cb2224a680fbbe24b8e12631
Josh Zylstra <zylstrajosh@gmail.com>
2017-04-18 09:24:15 -0700

    Trailing / : Running into problems.
    Trailing / - - stack overflow suggested fix.

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,7 +16,7 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
    const API_KEY = '620118ceafd2ae48d2ee3a48e8a683cd';
-   const ROOT_URL =  `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=imperial`;
+   const ROOT_URL =  `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=imperial/`;
    const url = `${ROOT_URL}&q=${city},us`;
    const request = axios.get(url);
    //will return a promise here
