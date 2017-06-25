2b21b005b5283cd79d30bfd1500919d6bcb82b02
Josh Zylstra <zylstrajosh@gmail.com>
2017-06-24 19:22:19 -0700

    Better understanding events
    actually writing code instead of simply reading the same shit, over and
    over again.

--- src/components/add_info.js
+++ src/components/add_info.js
@@ -8,11 +8,15 @@ import React from 'react';
 class AddInfo extends React.Component {
   constructor(props) {
     super(props);
+    this.actionMove = this.actionMove.bind(this);
+  }
+  actionMove() {
+    console.log("fun times");
   }
   render() {
     return (
       <div>
-      <h1>Fun Times</h1>
+      <h1 onClick={this.actionMove}>Fun Times</h1>
       <p>I hate being alone</p>
       <p>Fomo is real</p>
       <p>I love complaining</p>
