91eb8536c9a6b1295f24dc975353f16b3c96be08
zylstraj <colbertlove12@gmail.com>
2017-07-12 15:06:29 -0700

    Working on Redux.

--- src/components/news_stories.js
+++ src/components/news_stories.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchNews } from '../actions/index';
+import { fetchNews, learningStuff } from '../actions/index';
 import styles from './news_stories.css';
 
 class NewsStories extends React.Component {
   componentDidMount() {
     console.log('this would be a good time to call an action creator');
     this.props.fetchNews();
-    console.log(this.props)
+    console.log(this.props);
+    this.props.learningStuff();
   }
   createNewsList() {
     return this.props.news.slice(0,10).map((story) => {
@@ -40,6 +41,6 @@ function mapStateToProps(state) {
   return {news: state.news}
 }
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchNews }, dispatch);
+  return bindActionCreators({ fetchNews, learningStuff }, dispatch);
 }
 export default connect(mapStateToProps, mapDispatchToProps)(NewsStories);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import NewsReducer from './reducer_news';
 import WeatherReducer from './reducer_weather';
+import LearningReducer from './learning';
 
 const rootReducer = combineReducers({
   news: NewsReducer,
-  weather: WeatherReducer
+  weather: WeatherReducer,
+  learning: LearningReducer
 });
 export default rootReducer;
